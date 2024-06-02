4cd313dc5b140e1400fb436fc55f7677cb5ad4f8
christian.bergau <christian.gergau@arvato-scs.com>
2024-06-02 17:41:58 +0200

    initial commit

---
+++ src/main/kotlin/de/christianbergau/SortAction.kt
@@ -0,0 +1,10 @@
+package de.christianbergau.test_plugin.de.christianbergau.idea.plugin
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class SortAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        // TODO: insert action logic here
+    }
+}
